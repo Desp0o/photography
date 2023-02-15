d51b4bf8d9648433aa6df4471209dbb1bfe20916
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2023-02-15 15:19:24 -0800

    made react lightBox without library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/homePage";
-import Gallery from "./components/gallery";
+import Gallery1 from "./components/gallery";
 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
   return (
     <Routes>
       <Route exact path="*" element={<HomePage />} />
-      <Route exact path="/components/gallery" element={<Gallery />} />
+      <Route exact path="/components/gallery" element={<Gallery1 />} />
     </Routes>
   );
 }
--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,9 +1,128 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
+import './gallery.css'
 
-export default function Gallery(){
+import arrow from '../images/dirArrow.png'
+import close from '../images/closeBtn.png'
+
+
+export default function Galler1y(){
+    const [data, setData] = useState([]);
+    const [slideNumber, setSlideNumber] = useState(0)
+    const [openModal, setOpenModal] = useState(false)
+    const [num,setnum] = useState(0)
+
+    useEffect(() => {
+        fetch('https://desp0o.github.io/dataBase/dataBase.json')
+        .then(response => response.json())
+        .then(data => setData(data[num].list));
+        
+    }, [num]);
+
+    
+    //open modal
+    const handleOpenModal = (index) => {
+        setSlideNumber(index)
+        setOpenModal(true)
+    }
+
+    //open modal
+    const handleCloseModal = () => {
+        setOpenModal(false)
+    }
+
+    //previous slide
+    const prevSlide  = () => {
+        if(slideNumber === 0){
+            setSlideNumber(data.length - 1)
+        }else{
+            setSlideNumber(slideNumber - 1)
+        }
+    }
+
+    //next slide
+    const nextSlide = () => {
+        if(slideNumber === data.length - 1){
+            setSlideNumber(0)
+        }else{
+            setSlideNumber(slideNumber + 1)
+        }
+    }
+
+    // on arrow key donw changing slides
+    useEffect(()=>{
+        window.onkeydown = (e) =>{
+            if (e.keyCode === 37) {
+                prevSlide()
+              } else if(e.keyCode === 39){
+                nextSlide()
+              }
+        }
+    },[slideNumber])
+
+    
+function filter(number){
+    setnum(number)
+}
 
 
     return(
-        <div className=""></div>
+        <>
+        <h1 className='galleryTitle'>Gallery</h1>
+        <div className='filter'>
+            <p onClick={()=>filter('0')}>All</p>
+            <p onClick={()=>filter('1')}>B&W</p>
+        </div>
+
+        {
+            openModal &&
+                <div className='sliderWrap'>
+
+                {/* bg overlay */}
+                
+
+                    {/* button container */}
+                    <div className='buttonHandler'>
+                        <div className='overlay' onClick={handleCloseModal} />
+                        <img src={close} alt='close btn'  className='closeBtn' onClick={handleCloseModal} />
+                        <img src={arrow} alt='left arrow' className='arrowLeft' onClick={prevSlide} />
+                        <img src={arrow} alt='left arrow' className='arrowRight' onClick={nextSlide} /> 
+                    </div>
+                    
+
+                    {/* full screen image */}
+                    <div className='fullScreenImage'>
+                        <img src={data[slideNumber].image} alt='full' />
+                    </div>
+
+                </div>
+        }
+       
+        <div className='gallery-wrap'>
+        
+            {
+                data && data.map((slide, index) => {
+
+                    return(
+                        <div 
+                            className='thumbnail' 
+                            key={index}
+                            onClick={() => handleOpenModal(index)}>
+                                <img src={slide.image} alt='slide'/>
+                        </div>
+                    )
+                })
+            }
+
+                        
+        </div>
+
+        </>
     )
 }
+                
+        
+
+
+
+
+
