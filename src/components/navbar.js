c4246ef9819c71462bce337df1df64aa6b6c0adc
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2023-02-20 10:24:40 -0800

    small fixes in contact form

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,12 +2,18 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar(props){
     return(
-        <div className='w-full h-[80px] flex items-center justify-between pl-[50px] pr-[50px]'>
-            <p className="text-[20px] xs:text-[14px] text-[#FFF] font-[800] font-['Montserrat']">Shutterpics</p>
+        <div className='w-full h-[80px] flex items-center justify-between pl-[50px] xs:pl-[15px] pr-[50px] xs:pr-[15px] box-border'>
+            <p className="text-[30px] xs:text-[24px] text-[#FFF] font-[800] font-['Montserrat']">Shutterpics</p>
 
-            <div className='flex gap-[30px]'>
-                <Link to='/components/homePage'><p className="text-[16px] text-[#FFF] xs:text-[12px] font-[400] font-['Montserrat'] cursor-pointer">Home</p></Link>
-                <Link to={props.directLink}><p className="text-[16px] text-[#FFF] xs:text-[12px] font-[400] font-['Montserrat'] cursor-pointer">{props.linkName}</p></Link>
+            <div className='flex gap-[30px] xs:hidden'>
+                <Link to='/components/homePage'><p className="text-[16px] text-[#FFF]  font-[400] font-['Montserrat'] cursor-pointer">Home</p></Link>
+                <Link to={props.directLink}><p className="text-[16px] text-[#FFF]  font-[400] font-['Montserrat'] cursor-pointer">{props.linkName}</p></Link>
+            </div>
+
+            <div className='burgerMenu hidden xs:flex'>
+                <span className='bar'></span>
+                <span className='bar'></span>
+                <span className='bar'></span>
             </div>
         </div>
     )
