89c2347414512b206e3cde9b52e7b0eeced76dda
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2023-02-27 03:35:20 -0800

    added burger menu

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -2,7 +2,36 @@ import { useRef, useEffect, useState } from "react";
 import Navbar from "../navbar";
 import Form from "./form";
 
+// import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
+// const paypalStyle = {
+//   layout: 'vertical',
+//   color: 'black',
+//   shape: 'rect',
+//   label: 'paypal',
+//   with: 50,
+//   height: 30
+// };
+
 export default function Contact(){
+    // const createOrder = (data, actions) => {
+    //     return actions.order.create({
+    //       purchase_units: [
+    //         {
+    //           amount: {
+    //             value: "5.00",
+    //           },
+    //         },
+    //       ],
+    //     });
+    //   };
+
+    //   const onApprove = (data, actions) => {
+    //     return actions.order.capture().then(function (details) {
+    //       alert("Transaction completed by " + details.payer.name.given_name);
+    //     });
+    //   };
+
+
     return(
         <div className="w-full bg-[#212121] pb-[50px]">
             <Navbar
@@ -17,6 +46,14 @@ export default function Contact(){
                 <Form />
             </div>
 
+
+            {/* <PayPalScriptProvider options={{ "client-id": "AYTGR-FwzgVBLTU7THhgvrj6thwxhwx1DKnPl-sIlLntzXs7q4rTqfbSm0Yoq2x737SSLsQkVCdur8fc" }}>
+                <PayPalButtons
+                    createOrder={createOrder}
+                    onApprove={onApprove}
+                    style={paypalStyle}
+                />
+            </PayPalScriptProvider> */}
         </div>
 
         
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,27 @@
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+
+import close from '../images/closeBtn.png'
+import menuBars from '../images/menuBars.png'
 
 export default function Navbar(props){
+
+    const [burger, setBurger] = useState(false)
+    const [img, setImg] = useState(menuBars)
+    const [dashbaord, setDashboard] = useState('dashboard')
+
+    const hamburgerHandler = () => {
+        if(!burger){
+            setBurger(true)
+            setImg(close)
+            setDashboard('dashboard dashboard_active')
+        }else{
+            setBurger(false)
+            setImg(menuBars)
+            setDashboard('dashboard')
+        }
+    }
+
     return(
         <div className='w-full h-[80px] flex items-center justify-between pl-[50px] xs:pl-[15px] pr-[50px] xs:pr-[15px] box-border'>
             <p className="text-[30px] xs:text-[24px] text-[#FFF] font-[800] font-['Montserrat']">Shutterpics</p>
@@ -10,10 +31,16 @@ export default function Navbar(props){
                 <Link to={props.directLink}><p className="text-[16px] text-[#FFF]  font-[400] font-['Montserrat'] cursor-pointer">{props.linkName}</p></Link>
             </div>
 
-            <div className='burgerMenu hidden xs:flex'>
-                <span className='bar'></span>
-                <span className='bar'></span>
-                <span className='bar'></span>
+            <div 
+                className='burgerMenu hidden xs:flex'
+                onClick={hamburgerHandler}
+            >
+                <img src={img} alt='burger icons' />
+            </div>
+
+            <div className={dashbaord}>
+                <Link to='/components/homePage'><p className="text-[16px] text-[#FFF]  font-[400] font-['Montserrat'] cursor-pointer">Home</p></Link>
+                <Link to={props.directLink}><p className="text-[16px] text-[#FFF]  font-[400] font-['Montserrat'] cursor-pointer">{props.linkName}</p></Link>
             </div>
         </div>
     )
