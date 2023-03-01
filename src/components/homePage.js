e00917dae64fa793377534bc21607869f6ee3d27
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2023-03-01 05:58:47 -0800

    tets

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import mainBg from '../images/mainBg.webp'
+import mainBg from '../images/Tent-3.jpg'
 
 export default function HomePage(){
 
